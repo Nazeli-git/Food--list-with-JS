31d994a8bc88f4e08ec2cc09af2fdbc98f613323
Nazeli Hovhannisyan <nazeli.hovhannisyan23@gmail.com>
2024-09-27 17:53:19 +0400

    Added sale timer logic & functions with deadlian

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", () => {
+   //tabs start
   const tabHeaders = document.querySelectorAll(".tabheader__item");
   const tabHeadersParent = document.querySelector(".tabheader__items");
   const tabContents = document.querySelectorAll(".tabcontent");
-
-
 
   function hideTabContent(){
     tabContents.forEach(item => {
@@ -36,4 +35,57 @@ document.addEventListener("DOMContentLoaded", () => {
       })
     }
   });
+ //tabs end
+
+  //timer start
+
+    const deadline = "2024-09-30 24:00";
+
+    const setZero = n => n >= 0 && n < 10 ? `0${n}`: n;
+
+    function getTimeRemaining(endtime){
+      let days, hours, minutes, seconds;
+       const total = Date.parse(endtime) - Date.parse(new Date());
+     
+      if(total <= 0){
+        days = 0,
+        hours = 0,
+        minutes = 0,
+        seconds = 0
+      } else {
+        days = Math.floor(total / (1000 * 60 * 60 * 24));
+        hours = Math.floor((total / (1000 * 60 * 60 )% 24));
+        minutes = Math.floor((total / 1000 / 60) % 60);
+        seconds = Math.floor((total/1000)% 60);
+      }
+
+      return { total, days, hours, minutes, seconds }
+    }
+
+   function setTime(selector, endtime){
+      const timer = document.querySelector(selector);
+      const daysElem = timer.querySelector("#days");
+      const hoursElem = timer.querySelector ("#hours");
+      const minutesElem = timer.querySelector ("#minutes");
+      const secondsElem = timer.querySelector ("#seconds");
+        
+    const timerID = setInterval(updateTimer, 1000);
+ 
+    updateTimer();
+
+    function updateTimer(){
+      const {total, days, hours, minutes, seconds}= getTimeRemaining(endtime);
+      daysElem.innerHTML = setZero(days);
+      hoursElem.innerHTML = setZero(hours);
+      minutesElem.innerHTML = setZero(minutes);
+      secondsElem.innerHTML = setZero(seconds);
+
+      if(total <= 0){
+        clearInterval(timerID)
+      }
+    };
+   }; 
+    setTime(".timer", deadline)
+
+  //tamer end
 })
